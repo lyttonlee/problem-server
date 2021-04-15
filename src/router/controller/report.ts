67a3cb574fe459d226e9a454ad050e96f2b37bf8
lizairong <lzr3278@163.com>
2021-04-15 17:55:24 +0800

    fix: 获取客户端浏览器版本

--- src/router/controller/report.ts
+++ src/router/controller/report.ts
@@ -5,7 +5,8 @@ import koa = require('koa')
 const reportBug = async (ctx: koa.ParameterizedContext, next: koa.Next) => {
   try {
     const param: ReportBug = Object.assign(ctx.request.body, {
-      clientHost: ctx.request.headers.origin || ctx.request.headers.referer
+      clientHost: ctx.request.headers.origin || ctx.request.headers.referer,
+      brower: ctx.request.header['user-agent'] || ctx.request.headers['user-agent']
     })
     // console.log(param)
     const problem = await Problem.create(param)
